6ea471b7b0dda5b4411f979b599a8a36917873e6
Dijana <didimx.18@gmail.com>
2022-09-30 11:54:26 +0200

    form basic styling and some JS

---
+++ src/js/form.js
@@ -0,0 +1,7 @@
+var firstNameInput = document.querySelector("Name");
+var lastNameInput = document.querySelector("last_name");
+var emailInput = document.querySelector("Email");
+var phoneNrInput = document.querySelector("phone_nr");
+var ageInput = document.querySelector("age");
+
+var firstNameInput
